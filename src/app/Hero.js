ecea4471f690805e9057fa7d0ac1fc1ed8a439d0
fang-frank2020 <frankfangfrank@gmail.com>
2023-08-12 23:59:45 -0400

    feat: hero message

--- src/app/Hero.js
+++ src/app/Hero.js
@@ -1,5 +1,23 @@
 
 
 export default function Hero() {
-    
+    return(
+        <div>
+            <div className="HeroContainer">
+                <div className="LeftSide">
+                    <div className="Welcome">
+                        <p>Hi, I'm Frank!</p>
+                    </div>
+                    <div className="Intro">
+                        <span className="beginning">I am a </span>
+                        <span className="changed"></span>
+                        
+                    </div>
+                </div>
+                <div className="RightSide">
+
+                </div>
+            </div>
+        </div>
+    )
 }
