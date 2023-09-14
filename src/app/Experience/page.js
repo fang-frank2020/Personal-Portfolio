b0334fb5315c8759a66a47fd3b8ee4cf463dca41
fang-frank2020 <frankfangfrank@gmail.com>
2023-09-14 19:32:46 -0400

    IA experience

--- src/app/Experience/page.js
+++ src/app/Experience/page.js
@@ -11,6 +11,11 @@ export default function Experience() {
             <div className="Experience">
                 <Title titleName="Experience" />
                 <div className="ExperienceSectionWrapper">
+                    <ExperienceSection position="EECS 203(Discrete Mathematics) Instructional Aide" date="August 2023 - Present"
+                    description="I host weekly discussions and office hours, where I am supporting students in learning course content. Additionally,
+                    I also coordinate with the course staff to perform administrative duties such as making discussion slides and homework as well as reviewing
+                    exam material" />
+
                     <ExperienceSection position="Coding Instructor @ iD Tech Camps" date="June 2023 - August 2023"
                     description="I taught groups of 5-8 students basic programming principles where I first
                     taught visual programming through Scratch and then transitioned into Python.
